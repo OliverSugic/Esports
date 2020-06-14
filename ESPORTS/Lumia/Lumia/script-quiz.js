c702959d3ff5d101b27a2b1336b2f33040518d87
OliverSugic <osugic@gmail.com>
2020-06-14 13:34:54 +0200

    Quiz app finished

---
+++ ESPORTS/Lumia/Lumia/script-quiz.js
@@ -0,0 +1,104 @@
+const startbutton = document.getElementById('start-btn')
+startbutton.addEventListener('click',startGame)
+
+const nextButton = document.getElementById('next-btn')
+nextButton.addEventListener('click',() => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
+
+const questionConatinerElements = document.getElementById('question-container')
+
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffeldQuestion, currentQuestionIndex
+
+
+function startGame() {
+  startbutton.classList.add('hide')
+  questionConatinerElements.classList.remove('hide')
+  shuffeldQuestion = question.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0;
+  questionConatinerElements.classList.remove('hide')
+  setNextQuestion()
+}
+
+
+function setNextQuestion(){
+  resetState()
+  showNextQuestion(shuffeldQuestion[currentQuestionIndex])
+}
+
+function showNextQuestion(question){
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if(answer.correct){
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click',selectAnswser)
+    answerButtonsElement.appendChild(button)
+
+  });
+}
+
+function resetState(){
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while(answerButtonsElement.firstChild){
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+function selectAnswser(e){
+  const selectButton = e.target
+  const correct = selectButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button =>{
+    setStatusClass(button,button.dataset.correct)
+  })
+  if(shuffeldQuestion.length > currentQuestionIndex + 1){
+    nextButton.classList.remove('hide')
+  }else{
+    startbutton.innerText = 'Restart'
+    startbutton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct){
+  clearStatusClass(element)
+  if(correct){
+    element.classList.add('correct')
+  }
+  else{
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element){
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+} 
+
+const question = [
+  {
+    question: 'Whats this page about ?',
+    answers: [
+    {text: 'Esports', correct: true},
+    {text: 'Soccer', correct: false},
+    {text: 'Cars', correct: false},
+    {text: 'Tetris', correct: false}
+    ],
+
+    question: 'Whats the main event of the year ?',
+    answers: [
+    {text: 'CSGO Major', correct: true},
+    {text: 'VALORANT', correct: false},
+    {text: 'Rocket League', correct: false},
+    {text: 'War Thunder', correct: false}
+    ]
+  }
+]
